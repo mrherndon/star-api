a781e40d75cb10f80297eabd4c2db43428b05455
Raymond Herndon <rherndon@starsacramento.org>
2021-03-17 10:16:04 -0500

    sync adapters

--- requests/initialLoad.php
+++ requests/initialLoad.php
@@ -3,6 +3,7 @@
 echo json_encode([
     "schools" => schoolData::getRows(),
     "grades" => gradesData::getRows(),
+    "enrollingSchools" => schoolData::getCurrentlyEnrollingSchools($_POST['testing'] ?? false),
     "programListings" => programListingData::getCurrentlyEnrollingPrograms($_POST['testing'] ?? false),
     "schedules" => scheduleData::getCurrentlyEnrollingSchedules($_POST['testing'] ?? false),
     "extendedSchedules" => extendedScheduleData::getCurrentlyEnrollingExtendedSchedules($_POST['testing'] ?? false),
